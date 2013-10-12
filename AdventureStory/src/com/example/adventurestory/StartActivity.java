efe50432885b33771b49ab22ba60b5abd150b9ca
gaboriau <gaboriau@ualberta.ca>
2013-10-11 20:34:55 -0600

    Skeleton Built

--- AdventureStory/src/com/example/adventurestory/StartActivity.java
+++ AdventureStory/src/com/example/adventurestory/StartActivity.java
@@ -1,15 +1,44 @@
 package com.example.adventurestory;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
+	
+	private Button mkStory, publish, search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        
+        // Define Buttons
+        mkStory = (Button)findViewById(R.id.buttonMkStory);
+    	publish = (Button)findViewById(R.id.buttonPublish);
+    	search = (Button)findViewById(R.id.buttonSearch);
+    	
+    	// Check for user button clicks
+    	mkStory.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Bring up EditOptionActivity
+    		}
+    	});
+    	
+    	publish.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Call code for publishing a story
+    		}
+    	});
+    	
+    	search.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Bring up Story Search Activity
+    		}
+    	});
     }
 
 
@@ -21,3 +50,4 @@ public class StartActivity extends Activity {
     }
     
 }
+
