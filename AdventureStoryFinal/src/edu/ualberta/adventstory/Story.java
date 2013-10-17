f1684446e8742b24846fb13fb0560e88db45b4cf
lrolleman <lrollema@ualberta.ca>
2013-10-17 09:58:20 -0600

    added missing attribute and constructor to Story.java

--- AdventureStoryFinal/src/edu/ualberta/adventstory/Story.java
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/Story.java
@@ -2,17 +2,29 @@ package edu.ualberta.adventstory;
 
 import java.util.*;
 
+
 public class Story {
+	private Integer id;
 	private String title;
 	private String author;
 	private Option root;
 
+	//Like Option, overloaded constructors for when creating new story or loading from DB
 	public Story(String title, String author, Option root) {
+		this.id = null;
+		this.title = title;
+		this.author = author;
+		this.root = root;
+	}
+	public Story(Integer id, String title, String author, Option root) {
+		this.id = id;
 		this.title = title;
 		this.author = author;
 		this.root = root;
 	}
 	
+	public void setID(Integer i) {id = i;}
+	public Integer getID() {return id;}
 	public void setTitle(String t) {title = t;}
 	public String getTitle() {return title;}
 	public void setAuthor(String a) {author = a;}
@@ -20,7 +32,7 @@ public class Story {
 	public void setRoot(Option r) {root = r;}
 	public Option getRoot() {return root;}
 	
-	//return all options, see Option class for details
+	//return all options, see Option class for more details
 	public ArrayList<Option> getAllOptions() {
 		ArrayList<Option> ret = root.getAllOptions();
 		ret.add(root);
