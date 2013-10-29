a09b04a78e98d85d2a354b254fc332cb992c1d57
JoeyAndres <jandres@ualberta.ca>
2013-10-29 12:40:11 -0600

    AndroidManifest fixed.

--- AdventureStoryFinal/src/edu/ualberta/adventstory/StorySearchActivity.java
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/StorySearchActivity.java
@@ -15,7 +15,7 @@ public class StorySearchActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.story_search, menu);
+		getMenuInflater().inflate(R.menu.search, menu);
 		return true;
 	}
 
