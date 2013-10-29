162219fadf7421143af3eab5bf5bd13384ca3515
gaboriau <gaboriau@ualberta.ca>
2013-10-29 10:24:33 -0600

    Search Layout

--- AdventureStoryFinal/src/edu/ualberta/adventstory/MainActivity.java
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/MainActivity.java
@@ -1,25 +1,62 @@
 package edu.ualberta.adventstory;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button mkStory, publish, searchPage, searchStory, mkPage;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// this is my new feature
-		// normal dev
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Define Buttons
+        mkStory = (Button)findViewById(R.id.newStory);
+    	publish = (Button)findViewById(R.id.publish);
+    	searchPage = (Button)findViewById(R.id.findPage);
+    	
+    	// Check for user button clicks
+    	mkStory.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Bring up EditOptionActivity
+    			newStory();
+    		}
+    	});
+    	
+    	publish.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Call code for publishing a story
+    		}
+    	});
+    	
+    	searchPage.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			// Bring up Story Search Activity
+    		}
+    	});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+    
+	/* Called when the user clicks the Make a New Story button */
+	private void newStory(){
+		// Bring up Option Edit Activity
+		Intent logViewIntent = new Intent(this, OptionEditActivity.class);
+		startActivity(logViewIntent);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-		// this is my sencond feature
-	}
-
+    
 }
+
