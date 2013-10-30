ba4f808ab3b1b3dcb04142127b58183c51b3e2c5
lrolleman <lrollema@ualberta.ca>
2013-10-30 15:14:31 -0600

    added multimedia activity and layouts

---
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/AddMultimediaActivity.java
@@ -0,0 +1,24 @@
+package edu.ualberta.adventstory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMultimediaActivity extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	protected void onStart(){
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		
+	}
+	
+	private void setClickListeners() {
+		
+	}
+}
