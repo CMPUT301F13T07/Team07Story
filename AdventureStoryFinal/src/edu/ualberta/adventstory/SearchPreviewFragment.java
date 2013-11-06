3bc331d26e27a9f437d93083f0dfcff8b23c3937
hhoang1 <hhoang1@ualberta.ca>
2013-11-06 14:47:52 -0700

    fragment stuff

--- AdventureStoryFinal/src/edu/ualberta/adventstory/SearchPreviewFragment.java
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/SearchPreviewFragment.java
@@ -1,15 +1,21 @@
 package edu.ualberta.adventstory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SearchPreviewFragment extends Fragment {
+	
+	private TextView pageTitle;
+	private TextView pageText;
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
@@ -18,6 +24,8 @@ public class SearchPreviewFragment extends Fragment {
 			LinearLayout mLinearLayout = (LinearLayout) inflater.inflate(
 					R.layout.fragment_searchpreview, container, false);
 		
+			
+			
 			Button mSelect = (Button) mLinearLayout.findViewById(R.id.buttonselect);
 			Button mReturn = (Button) mLinearLayout.findViewById(R.id.buttonreturn);
 			
@@ -25,21 +33,29 @@ public class SearchPreviewFragment extends Fragment {
 			// Sets the button usage
 	    	mSelect.setOnClickListener(new OnClickListener(){
 	    		public void onClick(View v){
-	    			// Selects the selected page
-	    			
+	    			// Selects the selected page and goes to PageView Activity
+	    			// Need to add that it passes through the id
+	    			displayPage();
 	    		}
 	    	});
 	    	
 	    	mReturn.setOnClickListener(new OnClickListener(){
 	    		public void onClick(View v){
-	    			// Goes back to activity
-	    			
+	    			// End's fragment and goes back to activity
+	    			getActivity().getSupportFragmentManager().popBackStack();
 	    		}
 	    	});
 	    	
 			// Inflate the layout for this fragment
 			return mLinearLayout;
 		
+	}
+	
+	
+	
+	private void displayPage() {
+		Intent i = new Intent(getActivity(), PageViewActivity.class);
+		startActivityForResult(i, 0);
 	}
 	
 	
