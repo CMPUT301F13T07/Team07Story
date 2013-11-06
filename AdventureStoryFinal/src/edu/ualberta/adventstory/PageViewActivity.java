75caad6e608fd0488ee33cf34297c7ab08eb49e6
JoeyAndres <jandres@ualberta.ca>
2013-11-06 16:58:21 -0700

    Merging to AddMultimedia

--- AdventureStoryFinal/src/edu/ualberta/adventstory/PageViewActivity.java
+++ AdventureStoryFinal/src/edu/ualberta/adventstory/PageViewActivity.java
@@ -97,7 +97,6 @@ public class PageViewActivity extends ActivityExtended{
 			exit();
 		}
 	
-		Bundle info = intent.getExtras();
 		mPage = mDataSingleton.getCurrentPage();
 		mStory = mDataSingleton.getCurrentStory();
 		
