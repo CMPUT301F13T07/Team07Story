6a952e228148300ab8f12766b84ea1496f6f3e99
JoeyAndres <jandres@ualberta.ca>
2013-11-21 16:37:00 -0700

    size attributes

--- AdventureStoryFinal/src/edu/ualberta/multimedia/Picture.java
+++ AdventureStoryFinal/src/edu/ualberta/multimedia/Picture.java
@@ -9,14 +9,30 @@ import android.graphics.Matrix;
 import android.view.Display;
 import android.view.WindowManager;
 
-public class Picture extends MultimediaAbstract{	
-	public Picture(int id, int index, String file_dir){
-		super(id, index, file_dir);	
-	}
-	public Picture(int index, String file_dir) {
-		super(index, file_dir);
+public class Picture extends MultimediaAbstract{
+	public static enum IMAGESIZE{
+		SMALL,
+		MEDIUM,
+		LARGE
 	}
 	
+	protected IMAGESIZE mImageSize;
+	
+	public Picture(int id, int index, String file_dir){
+		super(id, index, file_dir);	
+		mImageSize = IMAGESIZE.MEDIUM;
+	}
+	
+	public Picture(int index, String file_dir) {
+		super(index, file_dir);
+		mImageSize = IMAGESIZE.MEDIUM;
+	}
+	
+	public Picture(int id, int index, String file_dir, IMAGESIZE imageSize){
+		super(id, index, file_dir);
+		mImageSize = imageSize;		
+	}
+
 	@Override
 	public boolean equals( Object obj ){
 		if( obj == null ) return false;
