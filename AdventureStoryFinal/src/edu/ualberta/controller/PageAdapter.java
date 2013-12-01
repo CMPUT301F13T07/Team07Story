b697e0023f448cf3db0ab7f0bb794d2b6a06e8f6
JoeyAndres <jandres@ualberta.ca>
2013-12-01 10:44:29 -0700

    some refactoring, I got refactoring notes in my dropbox.

--- AdventureStoryFinal/src/edu/ualberta/controller/PageAdapter.java
+++ AdventureStoryFinal/src/edu/ualberta/controller/PageAdapter.java
@@ -2,8 +2,10 @@ package edu.ualberta.controller;
 
 import java.util.ArrayList;
 
+import edu.ualberta.adventstory.TObserver;
 import edu.ualberta.data.DbManager;
 import edu.ualberta.multimedia.MultimediaAbstract;
+import edu.ualberta.multimedia.TObservable;
 import edu.ualberta.utils.Page;
 
 /**
@@ -17,10 +19,20 @@ public class PageAdapter{
 		return page.getMultimedia().isEmpty();		
 	}
 
+	/**
+	 * @param page is where we search for the Multimedia
+	 * @param multimediaID
+	 * @return Multimedia in <code>page</code> that matches multimediaID
+	 */
 	static public MultimediaAbstract getMultimediaByID(Page page, int multimediaID) {
 		return getMultimediaByID(page.getMultimedia(), multimediaID);
 	}
 	
+	/**
+	 * @param maList is the Multimedia list
+	 * @param multimediaID
+	 * @return Multimedia that matches id.
+	 */
 	static public MultimediaAbstract getMultimediaByID(ArrayList<MultimediaAbstract> maList, int multimediaID){
 		for(MultimediaAbstract ma:maList){
 			if(ma.getID() == multimediaID){
@@ -29,9 +41,8 @@ public class PageAdapter{
 		}
 		return null;
 	}
-	
+		
 	/**
-	 * 
 	 * @param maList ArrayList of Multimedia.
 	 * @param multimediID id of the Multimedia being deleted.
 	 * @return true if operation is success. false if Multimedia don't exist in <code>maList</code>.
@@ -47,5 +58,39 @@ public class PageAdapter{
 	
 	static public boolean deleteMultimediaByID(Page page, int multimediID){
 		return deleteMultimediaByID(page.getMultimedia(), multimediID);
+	}
+	
+	/**
+	 * Get the currently selected multimedia.
+	 * @param page
+	 * @return the first multimedia with selected attribute set.
+	 */
+	static public MultimediaAbstract getSelectedMultimedia(Page page){
+		for (MultimediaAbstract ma: page.getMultimedia()){
+			if(ma.getIsSelected()){
+				return ma;
+			}
+		}	
+		return null;
+	}
+	
+	/**
+	 * Clears the selection of given page.
+	 * @param page is the Page where mIsSelected attribute is cleared.
+	 */
+	static public void clearMultimediaSelection(Page page){
+		for( MultimediaAbstract ma: page.getMultimedia()){
+			ma.setIsSelected(false);
+		}
+	}
+	
+	/**
+	 * Since MultimediaAbstract is a model, a View can be attached to it. This
+	 * allows instant updating when an attribute changed.
+	 */
+	static public void attachView(Page page, TObserver view){
+		for( MultimediaAbstract ma: page.getMultimedia()){
+			ma.addObserver(view);
+		}
 	}
 }
